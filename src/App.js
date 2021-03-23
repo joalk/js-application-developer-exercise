c282e4a58dce69d96b57da7afb1432d14fbc1e47
joalk <joalkims@gmail.com>
2021-03-23 16:03:39 -0700

    refactored

--- src/App.js
+++ src/App.js
@@ -2,20 +2,30 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import backgrounds from './backgrounds.js';
-// import bunnyears from '../public/bunny-ears.mp4';
-import toboggan from '../public/toboggan.jpg'
+
 
 function App() {
-  const [bg, setBg] = useState('');
+
+  const [bg, setBg] = useState(logo);
+
+  useEffect(() => console.log(bg), [bg]);
+
+  const handleClick = (e) => {
+    setBg(`${e.target.value}`)
+  }
 
   let list = [<option value="please-choose-background">--Please Choose Background--</option>];
   for (let i = 0; i < backgrounds.length; i++) {
-    list.push(<option value="{backgrounds[i].name}">{backgrounds[i].name}</option>)
+    list.push(<option value={`${backgrounds[i].name}.${backgrounds[i].type}`}>{backgrounds[i].name}</option>)
   }
+
+
   return (
-    <div className="App-header" styles={{ backgroundImage:`url(${toboggan})`}}>
-      <img src={logo} alt="logo" />
-          <select name="select-bg" id="select-bg">
+    <div className="App-header">
+      {bg.includes('mp4') ? <video playsInline autoPlay muted loop>
+        <source src={`${process.env.PUBLIC_URL}${bg}`} type="video/mp4" />
+    </video> : <img src={`${process.env.PUBLIC_URL}${bg}`} alt="logo" />}
+          <select name="select-bg" id="select-bg" onChange={handleClick} value={bg}>
             {list}
           </select>
     </div>
