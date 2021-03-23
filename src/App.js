f7b63f72d67ef10938e0ad93c782c7b2b7da4169
joalk <joalkims@gmail.com>
2021-03-23 12:18:48 -0700

    modified selection list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import backgrounds from './backgrounds.js';
+// import bunnyears from '../public/bunny-ears.mp4';
+import toboggan from '../public/toboggan.jpg'
 
 function App() {
+  const [bg, setBg] = useState('');
+
+  let list = [<option value="please-choose-background">--Please Choose Background--</option>];
+  for (let i = 0; i < backgrounds.length; i++) {
+    list.push(<option value="{backgrounds[i].name}">{backgrounds[i].name}</option>)
+  }
   return (
-    <div className="App-header">
+    <div className="App-header" styles={{ backgroundImage:`url(${toboggan})`}}>
       <img src={logo} alt="logo" />
-      <select name="select-bg" id="select-bg"></select>
+          <select name="select-bg" id="select-bg">
+            {list}
+          </select>
     </div>
   );
 }
 
 export default App;
+
